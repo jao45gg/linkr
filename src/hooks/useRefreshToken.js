ea351a4f9f904a0c591debb91b6465f209b3b186
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-31 13:25:16 -0300

    fix: useRefreshToken setting avatar

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -9,8 +9,8 @@ const useRefreshToken = () => {
       const response = await axios.get("/refresh", {
         withCredentials: true,
       });
-      const { name, email, accessToken } = response.data;
-      setAuth({ name, email, accessToken });
+      const { name, avatar, accessToken } = response.data;
+      setAuth({ name, avatar, accessToken });
       return response.data.accessToken;
     } catch (err) {
       setAuth();
