753a9fb0a5c3e866f00a1816631a8fe98b361ef1
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-06-02 01:03:15 -0300

    feat: cookieCheck on auth context

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,13 +1,15 @@
 import { createContext, useState } from "react";
 import React from "react";
+import useCookieCheck from "../hooks/useCookieCheck";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+  const cookiesAccepted = useCookieCheck();
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ auth, setAuth, cookiesAccepted }}>
       {children}
     </AuthContext.Provider>
   );
