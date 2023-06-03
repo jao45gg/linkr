1690c45fa4d96b1f32634c84b7dc2ff9fabfec69
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-06-03 17:36:37 -0300

    a bunch refator

---
+++ src/components/header/Input.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { DebounceInput } from "react-debounce-input";
+import { useNavigate } from "react-router-dom";
+
+import useAxiosPrivate from "../../hooks/useAxiosPrivate.js";
+import { UserContainer } from "../../styles/HeaderBarStyle.js";
+
+const Input = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [usersData, setUsersData] = useState([]);
+  const axios = useAxiosPrivate();
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        if (name !== "") {
+          const data = await axios.get(`/users/getByName/${name}`);
+          setUsersData(data.data);
+        }
+      } catch (err) {
+        setUsersData([]);
+      }
+    }
+    getUsers();
+  }, [name, axios]);
+  return (
+    <div className="input-menu">
+      <div className="search">
+        <DebounceInput
+          type="text"
+          placeholder="Search for people"
+          minLength={3}
+          debounceTimeout={300}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <img src="/search.svg" alt="search" />
+      </div>
+      <div className="users">
+        {usersData.length > 0 &&
+          usersData.map((m, index) => (
+            <UserContainer
+              onClick={() => {
+                navigate(`/user/${m.id}`);
+                location.reload();
+              }}
+              key={index}>
+              <img className="searchImg" src={m.picture} />
+              <h1>{m.name}</h1>
+            </UserContainer>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/styles/HeaderBarStyle.js
@@ -0,0 +1,153 @@
+import styled from "styled-components";
+
+export const Content = styled.main`
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+export const HStyle = styled.div`
+  height: 72px;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #151515;
+  @media (max-width: 520px) {
+    margin-bottom: 72px;
+  }
+`;
+
+export const HContent = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  div {
+    display: flex;
+    align-items: center;
+    a {
+      font-family: "Passion One";
+      font-size: 49.976px;
+      font-weight: 700;
+      color: #fff;
+      margin-right: 30px;
+    }
+    img {
+      width: 53px;
+      height: 53px;
+      border-radius: 26.5px;
+      margin-left: 30px;
+      cursor: pointer;
+    }
+  }
+
+  .search {
+    width: 100%;
+    border-radius: 8px;
+    height: 45px;
+    border: none;
+    padding-left: 17px;
+    box-sizing: border-box;
+    background: #ffffff;
+  }
+  .search input {
+    width: 85%;
+    height: 100%;
+    border: none;
+    background: none;
+    outline: none;
+    font-family: "Lato";
+    font-size: 19px;
+    line-height: 23px;
+    color: #c6c6c6;
+  }
+  .search img {
+    width: 26px;
+    height: 22px;
+    margin-left: 10px;
+    cursor: pointer;
+  }
+
+  .input-menu {
+    width: 100%;
+    max-width: 563px;
+    border-radius: 8px;
+
+    max-height: 176px;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: #e7e7e7;
+    :hover {
+      .users {
+        display: flex !important;
+      }
+    }
+    .users {
+      width: 100%;
+      display: none;
+      flex-direction: column;
+      overflow-y: scroll;
+    }
+  }
+  @media (max-width: 520px) {
+    .input-menu {
+      position: absolute;
+      top: 75px;
+    }
+  }
+`;
+
+export const Drop = styled.img`
+  width: 20px !important;
+  height: 20px !important;
+  position: relative;
+  cursor: pointer;
+  rotate: ${({ menuActive }) => (menuActive ? "180deg" : "0deg")} !important;
+`;
+export const DropMenu = styled.div`
+  position: absolute;
+  top: 60px;
+  right: 0;
+
+  width: 150px;
+  height: 47px;
+  padding: 8px;
+  border-radius: 0px 0px 20px 20px;
+
+  background: #171717;
+  cursor: pointer;
+  display: ${({ menuActive }) => (menuActive ? "block" : "none")} !important;
+  P {
+    margin-top: 12px;
+    text-align: center;
+    font-family: "Lato";
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    color: #fff;
+  }
+`;
+
+export const UserContainer = styled.div`
+  min-height: 66px;
+  width: 100%;
+  .searchImg {
+    height: 39px;
+    width: 39px;
+  }
+  h1 {
+    font-family: "Lato";
+    font-weight: 400;
+    margin-left: 12px;
+    font-size: 19px;
+    color: #515151;
+  }
+
+  :hover {
+    cursor: pointer;
+  }
+`;
