031437332dd2aa292fe9847b06b389b0714999d8
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-06-04 19:28:34 -0300

    hash page

---
+++ src/routes/Trending.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Post from "../components/Post.js";
+import useAuth from "../hooks/useAuth.js";
+import LoadingPage from "../components/loadings/LoadingPage.js";
+import useAxiosPrivate from "../hooks/useAxiosPrivate.js";
+import { Container, Titulo, Posts, Aside } from "../styles/TimeLineStyle.js";
+
+const Trending = () => {
+  const params = useParams();
+  const [hashDetail, setHashDetail] = useState([]);
+  const { auth } = useAuth();
+  const axios = useAxiosPrivate();
+
+  useEffect(() => {
+    const getHash = async () => {
+      try {
+        const hashtags = await axios.get(`/hashtags/${params.hashtag}`);
+        if (hashtags.data.length > 0) {
+          console.log(hashtags.data);
+          setHashDetail(hashtags.data);
+        } else {
+          setHashDetail(undefined);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getHash();
+  }, [axios, params]);
+
+  return (
+    <Container>
+      <Titulo>
+        <h1>Trending</h1>
+      </Titulo>
+      <Posts>
+        <Aside>
+          {hashDetail !== undefined ? (
+            hashDetail.map((item) => (
+              <Post
+                key={item.id}
+                id={item.id}
+                link={item.link}
+                description={item.description}
+                userId={auth.id}
+                likes={[]}
+                picture={item.user_picture}
+                userName={item.user_name}
+                userPostId={item.user_id}
+                token={auth.accessToken}
+                liked={"userLiked"}
+                Refresh={() => {}}
+              />
+            ))
+          ) : (
+            <LoadingPage />
+          )}
+        </Aside>
+      </Posts>
+    </Container>
+  );
+};
+export default Trending;
