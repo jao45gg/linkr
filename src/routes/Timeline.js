55027a17e092da2074d9bac31e84ee0e808652ac
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-06-09 19:19:03 -0300

    feat: new post rendering on client

--- src/routes/Timeline.js
+++ src/routes/Timeline.js
@@ -30,6 +30,7 @@ export default function Timeline({ setNewRequest }) {
   const [hasMore, setHasMore] = useState(false);
   const { auth } = useAuth();
   const axiosPrivate = useAxiosPrivate();
+  const [subtract, setSubtract] = useState(0);
 
   useEffect(() => {
     page = 1;
@@ -37,6 +38,7 @@ export default function Timeline({ setNewRequest }) {
   }, []);
 
   function Refresh() {
+    setSubtract(0);
     const promise = axiosPrivate.get(`/posts/${page} ${offset}`);
     promise.then((res) => {
       if (res.data[res.data.length - 1]?.id !== data[data.length - 1]?.id) {
@@ -55,7 +57,8 @@ export default function Timeline({ setNewRequest }) {
       const promise = axiosPrivate.get(`/posts/newPosts/${data[0]?.id}`);
       promise
         .then((res) => {
-          if (data.length / page + res.data > 10) {
+          console.log(data.length, page, res.data);
+          if ((data.length - subtract) / page + res.data > 10) {
             setOffset(data.length / page + res.data - 10);
           } else if (data.length < 10) {
             Refresh();
@@ -81,8 +84,22 @@ export default function Timeline({ setNewRequest }) {
 
     const promise = axiosPrivate.post("/posts/", form);
     promise
-      .then(() => {
-        Refresh();
+      .then((res) => {
+        const newData = [...data];
+        newData.unshift({
+          commentsCount: "0",
+          description: form.description,
+          id: res.data.post_id,
+          likes: [],
+          link: form.url,
+          shares: [],
+          userLiked: false,
+          user_id: auth.id,
+          user_name: auth.name,
+          user_picture: auth.avatar,
+        });
+        setData(newData);
+        setSubtract(1);
       })
       .catch((res) => {
         alert("There was an error publishing your link");
@@ -135,24 +152,31 @@ export default function Timeline({ setNewRequest }) {
         <Aside>
           {erro === true ? (
             <ErrorServer
-              message={"An error occured while trying to fetch the posts, please refresh the page"}
+              message={
+                "An error occured while trying to fetch the posts, please refresh the page"
+              }
             />
           ) : data.length === 0 ? (
-            <ErrorServer message={"No posts found from your friends"} data-test="message" />
+            <ErrorServer
+              message={"No posts found from your friends"}
+              data-test="message"
+            />
           ) : data !== undefined ? (
             <>
               <ButtonNewposts
                 display={offset > 0 ? "flex" : "none"}
                 onClick={() => {
                   setOffset(0);
-                }}>
+                }}
+              >
                 {`${offset} new posts, load more!`}
                 <svg
                   width="22"
                   height="16"
                   viewBox="0 0 22 16"
                   fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     d="M11.2391 4.19004e-06C15.4598 4.19004e-06 18.9272 3.10714 19.513 7.14285H22L17.8152 11.9048L13.6304 7.14285H16.4043C16.1369 5.9775 15.4804 4.93688 14.5423 4.19091C13.6042 3.44495 12.4397 3.03771 11.2391 3.03571C9.50543 3.03571 7.975 3.88095 7.00652 5.15476L4.96196 2.83333C5.74453 1.94233 6.70962 1.22848 7.79235 0.739766C8.87507 0.251055 10.0503 -0.00118567 11.2391 4.19004e-06ZM10.7609 16C6.55217 16 3.07283 12.8928 2.48696 8.85714H0L4.18478 4.09524C5.5837 5.67857 6.97065 7.27381 8.36957 8.85714H5.59565C5.86314 10.0225 6.51955 11.0631 7.45769 11.8091C8.39583 12.555 9.56028 12.9623 10.7609 12.9643C12.4946 12.9643 14.025 12.119 14.9935 10.8452L17.038 13.1667C16.2562 14.0586 15.2913 14.773 14.2084 15.2618C13.1255 15.7506 11.9498 16.0023 10.7609 16Z"
                     fill="white"
@@ -166,7 +190,8 @@ export default function Timeline({ setNewRequest }) {
                   page++;
                   Refresh();
                 }}
-                loader={<div key={0}>Carregando...Carregando...</div>}>
+                loader={<div key={0}>Carregando...Carregando...</div>}
+              >
                 {data.map((item) => (
                   <Post
                     key={item.id}
