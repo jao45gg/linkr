761e2ba3d658cdf483abb4f7e39dad0399653e47
tais <tais15oliva@gmail.com>
2023-06-09 17:31:20 -0300

    Deletando o post e os seus reposts

--- src/components/Post.js
+++ src/components/Post.js
@@ -277,6 +277,7 @@ export default function Post({
                 link={link}
                 description={description}
                 userId={userPostId}
+                shares={shares}
               />
               <Text>
                 <div>
